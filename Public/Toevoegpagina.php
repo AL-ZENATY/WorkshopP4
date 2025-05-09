41779f8ada738f3a3211ff3a964b5f0a12b01d6f
Jarne Kampshof <Jarne.Kampshof@student.graafschapcollege.nl>
2025-05-09 11:26:34 +0200

    toevoeg

--- Public/Toevoegpagina.php
+++ Public/Toevoegpagina.php
@@ -19,11 +19,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <h1>Klant Toevoegen</h1>
 <form method="post">
-    <label>voornaam: <input type="text" name="vak" required></label><br>
-    <label>tussenvoegsel: <input type="text" name="periode" required></label><br>
-    <label>achternaam: <input type="text" name="cijfer" required></label><br>
-    <label>email: <input type="text" name="vak" required></label><br>
-    <label>telefoonnummer: <input type="text" name="periode" required></label><br>
-    <label>adres: <input type="text" name="cijfer" required></label><br>
+    <label>voornaam: <input type="text" name="voornaam" required></label><br>
+    <label>tussenvoegsel: <input type="text" name="tussenvoegsel" required></label><br>
+    <label>achternaam: <input type="text" name="achternaam" required></label><br>
+    <label>email: <input type="text" name="email" required></label><br>
+    <label>telefoonnummer: <input type="text" name="telefoonnummer" required></label><br>
+    <label>adres: <input type="text" name="adres" required></label><br>
     <button type="submit">Toevoegen</button>
 </form>
